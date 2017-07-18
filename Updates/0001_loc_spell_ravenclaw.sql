213c047f128ef2ebe255636326af190bbc69fa73
M2us <Dead_Maus@mail.ru>
2017-07-18 18:56:56 +0300

    [0001] Fixed Ravenclaw spell name in name_loc8

---
+++ Updates/0001_loc_spell_ravenclaw.sql
@@ -0,0 +1,19 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Прикосновение Когтя Ворона' 
+WHERE `id` = 3263;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Сглаз Когтя Ворона' 
+WHERE `id` = 7655;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Сглаз Когтя Ворона' 
+WHERE `id` = 7656;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Сглаз Когтя Ворона' 
+WHERE `id` = 7657;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Сглаз Когтя Ворона - удаление' 
+WHERE `id` = 8320;
