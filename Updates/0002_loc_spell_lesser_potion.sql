f23211b7b9b6188282d6bc4e58af2dd5382e5133
M2us <Dead_Maus@mail.ru>
2017-07-20 21:02:15 +0300

    [0002] Fixed lesser potions name in name_loc8

---
+++ Updates/0002_loc_spell_lesser_potion.sql
@@ -0,0 +1,59 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье силы' 
+WHERE `id` = 2367;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье интеллекта' 
+WHERE `id` = 2376;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье ловкости' 
+WHERE `id` = 2374;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье ловкости' 
+WHERE `id` = 3157;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье ловкости' 
+WHERE `id` = 3158;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье силы' 
+WHERE `id` = 3161;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье силы' 
+WHERE `id` = 3162;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье интеллекта' 
+WHERE `id` = 3165;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье интеллекта' 
+WHERE `id` = 3166;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье духа' 
+WHERE `id` = 7847;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье духа' 
+WHERE `id` = 7851;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье интеллекта' 
+WHERE `id` = 13363;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье ловкости' 
+WHERE `id` = 13364;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Малое зелье силы' 
+WHERE `id` = 13371;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Формула чар для наручей – отражение II' 
+WHERE `id` = 13647;
