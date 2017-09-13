30d44dedbc126a7fc6cdc8b44d16167c94ea4f9d
M2us <Dead_Maus@mail.ru>
2017-09-13 18:38:14 +0300

    [0013] Fixed spell names id 8733, 8740, 8734, 8735 in loc8

---
+++ Updates/0013_loc_spell_blessing_of_blackfathom.sql
@@ -0,0 +1,15 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Благословение Непроглядной Пучины' 
+WHERE `id` = 8733;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='zzOLDБлагословение Непроглядной Пучины' 
+WHERE `id` = 8740;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Поддержание Непроглядной Пучины' 
+WHERE `id` = 8734;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Телепорт Непроглядной Пучины' 
+WHERE `id` = 8735;
