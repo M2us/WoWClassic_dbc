33af90d8862cc0a698029842b811c1bcd92c45b1
M2us <Dead_Maus@mail.ru>
2017-09-15 19:15:40 +0300

    [0021] Fixed Health Funnel description in description_loc8

---
+++ Updates/0021_loc_spell_health_funnel.sql
@@ -0,0 +1,2 @@
+UPDATE `locales_spell` SET 
+	`description_loc8` = REPLACE(`description_loc8`, 'если заклинатель поддерживает заклинание.', 'до тех пор, пока заклинатель поддерживает эту способность.');
