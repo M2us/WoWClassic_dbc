eaf8c18700345cad6dd93262cacbbdc17b2e0bd8
M2us <Dead_Maus@mail.ru>
2017-09-16 19:19:34 +0300

    [0024] Moss Covered Feet name and auradescription in loc8

---
+++ Updates/0024_loc_spell_moss_covered_feet.sql
@@ -0,0 +1,8 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Замшелые ноги', 
+	`auradescription_loc8`='Вы можете упасть при получении удара.' 
+WHERE `id` = 6870;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Замшелые ноги - дополнительный эффект' 
+WHERE `id` = 6871;
