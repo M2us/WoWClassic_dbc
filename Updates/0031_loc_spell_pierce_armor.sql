cbc99ac5b19a68a2b57cee944239ae3bec67d63c
M2us <Dead_Maus@mail.ru>
2017-09-17 19:23:49 +0300

    [0031] Fixed Pierce Armor name and description in loc8

---
+++ Updates/0031_loc_spell_pierce_armor.sql
@@ -0,0 +1,9 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Проколотая броня', 
+	`description_loc8`='Ослабляет броню противника на $s1% на $d.' 
+WHERE `id` = 6016;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Проколотая броня', 
+	`description_loc8`='Ослабляет броню противника на $s1% на $d.' 
+WHERE `id` = 12097;
