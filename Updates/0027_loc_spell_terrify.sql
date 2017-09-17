9fdb3ed01fb267a8ff141f59eb76872a5ea453d9
M2us <Dead_Maus@mail.ru>
2017-09-17 18:22:57 +0300

    [0027] Fixed Terrify auradescription in auradescription_loc8

---
+++ Updates/0027_loc_spell_terrify.sql
@@ -0,0 +1,7 @@
+UPDATE `locales_spell` SET 
+	`auradescription_loc8`='Бегство в страхе.' 
+WHERE `id` = 7399;
+
+UPDATE `locales_spell` SET 
+	`auradescription_loc8`='Потеря $s1 ед. здоровья раз в $t1 сек.$BОглушение.' 
+WHERE `id` = 4538;
