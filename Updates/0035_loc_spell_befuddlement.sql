2faf7a3ddffef5af70296affc2e0bc6815c4c7b1
M2us <Dead_Maus@mail.ru>
2017-09-18 19:05:31 +0300

    [0035] Fixed Befuddlement description and auradescription in loc8

---
+++ Updates/0035_loc_spell_befuddlement.sql
@@ -0,0 +1,4 @@
+UPDATE `locales_spell` SET 
+	`description_loc8`='Снижает скорость произнесения заклинаний противника на $s1% на $d.', 
+	`auradescription_loc8`='Скорость произнесения заклинаний снижена на $s1%.' 
+WHERE `id` = 8140;
