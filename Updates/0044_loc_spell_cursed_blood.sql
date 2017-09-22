0b4c9ac8fb713f7b89383326211e91a280ed76c4
M2us <Dead_Maus@mail.ru>
2017-09-22 18:27:24 +0300

    [0044] Fixed Cursed Blood description and auradescription in loc8

---
+++ Updates/0044_loc_spell_cursed_blood.sql
@@ -0,0 +1,4 @@
+UPDATE `locales_spell` SET 
+	`description_loc8`='Понижает интеллект ближайших противников на $s1 ед. на $d.', 
+	`auradescription_loc8`='Интеллект снижен на $s1.' 
+WHERE `id` = 8267;
