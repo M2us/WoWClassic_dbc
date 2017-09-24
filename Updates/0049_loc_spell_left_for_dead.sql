ddcfbeeb6f542ad0f30d7fdcb89593d3c81c224a
M2us <Dead_Maus@mail.ru>
2017-09-24 19:57:55 +0300

    [0049] Fixed Left for Dead description and auradescription in loc8

---
+++ Updates/0049_loc_spell_left_for_dead.sql
@@ -0,0 +1,7 @@
+UPDATE `locales_spell` SET 
+	`auradescription_loc8`='Ложная смерть.$BНевозможно атаковать или произносить заклинания.' 
+WHERE `id` = 8359;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наносит врагу $s1 ед. урона и вынуждает его притвориться мертвым на $8359d.' 
+WHERE `id` = 8555;
