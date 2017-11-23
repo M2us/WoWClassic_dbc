6a82b8f5eea660e1c943c3589b81d269f67c06b8
M2us <Dead_Maus@mail.ru>
2017-11-23 19:48:21 +0500

    [0065] Fixed racial bonus description in loc8

---
+++ Updates/0065_loc_spell_racial_bonus.sql
@@ -0,0 +1,7 @@
+UPDATE `locales_spell` SET
+	`description_loc8`='Увеличивает вероятность уклонения на $s1%.'
+WHERE `id` = 20582;
+
+UPDATE `locales_spell` SET
+	`description_loc8`='Увеличивает наносимый урон питомцами охотников, а также прислужниками чернокнижников на $s1%.'
+WHERE `id` = 21563;
