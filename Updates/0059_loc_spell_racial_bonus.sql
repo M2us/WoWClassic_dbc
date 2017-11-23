91e490131a5fa8dbc95fbe5f2bdbd81eb94392aa
M2us <Dead_Maus@mail.ru>
2017-11-23 17:12:51 +0500

    [0059] Fixed racial bonus description in loc8

---
+++ Updates/0059_loc_spell_racial_bonus.sql
@@ -0,0 +1,127 @@
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает ваше сопротивление магии льда на $s1 ед.' 
+WHERE `id` = 4080;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает ваше сопротивление силам природы на $m1 ед.' 
+WHERE `id` = 4081;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Значительно повышает скорость бега на $d.' 
+WHERE `id` = 4083;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает ваше сопротивление темной магии на $s1 ед.' 
+WHERE `id` = 4084;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает продолжительность подводного дыхания на $s1% дольше обычной.' 
+WHERE `id` = 5227;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает дух на $s1%.' 
+WHERE `id` = 20598;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает получаемую репутацию на $s1%.' 
+WHERE `id` = 20599;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения одноручными и двуручными мечами на $s1 ед.' 
+WHERE `id` = 20597;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения одноручным и двуручным дробящем оружием на $s1 ед.' 
+WHERE `id` = 20864;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает сопротивление магии льда на $s1 ед.' 
+WHERE `id` = 20596;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения огнестрельным оружием на $s1 ед.' 
+WHERE `id` = 20595;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Превращение в огонька после смерти, что повышает скорость передвижения на $20584s1%.' 
+WHERE `id` = 20585;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает сопротивление силам природы на $s1 ед.' 
+WHERE `id` = 20551;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает сопротивление силам природы на $s1 ед.' 
+WHERE `id` = 20583;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает интеллект на $s1%.' 
+WHERE `id` = 20591;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает сопротивление тайной магии на $s1 ед.' 
+WHERE `id` = 20592;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык \"Инженерное дело\" на $s1 ед.' 
+WHERE `id` = 20593;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает наносимый урон питомцами охотников, а также прислужниками чернокнижников на $s1%.' 
+WHERE `id` = 20575;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает наносимый урон питомцами охотников, а также прислужниками чернокнижников на $s1%.' 
+WHERE `id` = 20576;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает силу атаки ближнего боя на $s1% на $23234d. и снижает эффективность получаемого вами исцеления на $23230s1% на $23230d.' 
+WHERE `id` = 20572;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения топорами и двуручными топорами на $s1 ед.' 
+WHERE `id` = 20574;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает шанс сопротивления эффектам оглушения на $s1%.' 
+WHERE `id` = 20573;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='При активации придает невосприимчивость к заклинаниям \"Подчинение\", \"Страх\" и \"Сон\". Можно использовать даже тогда, когда эффекты этих заклинаний уже действует на вас. Длительность – $d.' 
+WHERE `id` = 7744;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает сопротивление темной магии на $s1 ед.' 
+WHERE `id` = 20579;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает базовый запас здоровья на $s1%.' 
+WHERE `id` = 20550;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык \"Травничество\" на $s1 ед.' 
+WHERE `id` = 20552;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает скорость вашей атаки на $26635m1% - $26635M1%. При полном запасе здоровья увеличение составляет $26635m1%, а при значительных ранениях и в состоянии берсерка увеличение достигает $26635M1%. Время действия – $26635d.' 
+WHERE `id` = 26296;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает скорость вашей атаки на $26635m1% - $26635M1%. При полном запасе здоровья увеличение составляет $26635m1%, а при значительных ранениях и в состоянии берсерка увеличение достигает $26635M1%. Время действия – $26635d.' 
+WHERE `id` = 26297;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает уровень восполнения здоровья на $s1%. $s2% восполнения здоровья может продолжаться во время битвы.' 
+WHERE `id` = 20555;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения луком на $s1 ед.' 
+WHERE `id` = 26290;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает навык владения метательным оружием на $s1 ед.' 
+WHERE `id` = 20558;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Увеличивает урон, наносимый животным, на $s1%.' 
+WHERE `id` = 20557;
