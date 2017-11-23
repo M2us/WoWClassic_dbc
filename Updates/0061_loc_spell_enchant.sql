e4282e2a5850bcf60eaf7fecb3a434b263f7c824
M2us <Dead_Maus@mail.ru>
2017-11-23 17:38:02 +0500

    [0061] Fixed some enchant description in loc8

---
+++ Updates/0061_loc_spell_enchant.sql
@@ -0,0 +1,23 @@
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наложение на наручи чар, повышающих дух на 3.' 
+WHERE `id` = 7859;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наложение на наручи чар, повышающих выносливость на 3.' 
+WHERE `id` = 13501;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наложение на наручи чар, повышающих силу на 3.' 
+WHERE `id` = 13536;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Обучает наложению на наручи чар, повышающих силу на 3.' 
+WHERE `id` = 13537;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наложение на наручи чар, повышающих интеллект на 3 ед.' 
+WHERE `id` = 13622;
+
+UPDATE `locales_spell` SET 
+	`description_loc8`='Наложение на наручи чар, повышающих ловкость на $13823s1 ед.' 
+WHERE `id` = 22105;
