91c4a761be212294cf6a218aa26ea67a67c8896c
M2us <Dead_Maus@mail.ru>
2017-12-01 14:09:17 +0500

    Third release. ruRU version 23.11.2017

--- Updates/0065_loc_spell_racial_bonus.sql
+++
@@ -1,7 +0,0 @@
-UPDATE `locales_spell` SET
-	`description_loc8`='Увеличивает вероятность уклонения на $s1%.'
-WHERE `id` = 20582;
-
-UPDATE `locales_spell` SET
-	`description_loc8`='Увеличивает наносимый урон питомцами охотников, а также прислужниками чернокнижников на $s1%.'
-WHERE `id` = 21563;
