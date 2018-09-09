46736ebfa19ea05d630d8b1b7a1474446080b261
M2us <Dead_Maus@mail.ru>
2018-09-10 00:46:01 +0300

    [0069] Fixed Master Swordsmith and Master Axesmith name in loc8

---
+++ Updates/0069_loc_spell.sql
@@ -0,0 +1,7 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Мастер школы Меча' 
+WHERE `id` IN (17039, 17042);
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Мастер школы Топора' 
+WHERE `id` IN (17041, 17043);
