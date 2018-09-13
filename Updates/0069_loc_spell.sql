04be1114f40d0aea6f648bdc0cde765bb51e5a3b
M2us <Dead_Maus@mail.ru>
2018-09-13 03:06:05 +0300

    Fifth release. ruRU version 09.09.2018

--- Updates/0069_loc_spell.sql
+++
@@ -1,7 +0,0 @@
-UPDATE `locales_spell` SET 
-	`name_loc8`='Мастер школы Меча' 
-WHERE `id` IN (17039, 17042);
-
-UPDATE `locales_spell` SET 
-	`name_loc8`='Мастер школы Топора' 
-WHERE `id` IN (17041, 17043);
