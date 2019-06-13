7d5b300d1f74a217e481e5a1e26aee911ee83d94
M2us <Dead_Maus@mail.ru>
2019-06-13 22:26:22 +0300

    [0071] Fixed paladin talant name 'Lasting Judgement' in loc8

---
+++ Updates/0071_loc_spell.sql
@@ -0,0 +1,11 @@
+UPDATE `locales_spell` SET 
+	`name_loc8`='Долгое правосудие' 
+WHERE `id` = 20359;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Долгое правосудие' 
+WHERE `id` = 20360;
+
+UPDATE `locales_spell` SET 
+	`name_loc8`='Долгое правосудие' 
+WHERE `id` = 20361;
